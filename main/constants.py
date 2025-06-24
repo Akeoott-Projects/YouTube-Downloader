54182df8356db27464ffe6cbd266eef4b68df5d0
Akeoott <amejanin00@gmail.com>
2025-06-25 00:26:45 +0200

    fix: update version information to 1.0.3-beta

--- main/constants.py
+++ main/constants.py
@@ -1,5 +1,5 @@
 # constants.py for contant variables
-PROGRAM_VERSION = '1.0.2-beta'
+PROGRAM_VERSION = '1.0.3-beta'
 
 INFO_TITLE = f"YouTube Downloader v{PROGRAM_VERSION}"
 WARNING_TITLE = f"WARNING - YouTube Downloader v{PROGRAM_VERSION}"
